4255d1452f58bde0879c1d2129da3299f130cb2f
foster95 <alicerosefoster95@gmail.com>
2026-01-29 16:26:48 +0000

    completed and finished main navigation bar and mobile navigation bar including css styling

--- the_tavern/settings.py
+++ the_tavern/settings.py
@@ -18,10 +18,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'),)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
