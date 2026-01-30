7051d5748c74536f15cef2b1fbb7198afe24a1aa
foster95 <alicerosefoster95@gmail.com>
2026-01-30 12:32:59 +0000

    fix issue with fixtures and media files that wasn't allowing product images to show up

--- the_tavern/settings.py
+++ the_tavern/settings.py
@@ -21,7 +21,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'),)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
