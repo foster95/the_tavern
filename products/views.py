3b5f6530528d0b476be8e1d8c660bfd9c20561b6
foster95 <alicerosefoster95@gmail.com>
2026-02-02 15:49:48 +0000

    add sorting ability and tags to product catalog

--- products/views.py
+++ products/views.py
@@ -1,70 +1,74 @@
 from django.contrib import messages
-from django.shortcuts import redirect, reverse, render, get_object_or_404
+from django.shortcuts import redirect, reverse, render
 from django.db.models import Q
 from django.db.models.functions import Lower
+
 from .models import Product, Category
 
-# Create your views here.
 
 def product_list(request):
     """ Show all products """
 
-    products = Product.objects.all().order_by('name')  # ✅ always safe
+    products = Product.objects.all()
+    all_categories = Category.objects.all().order_by('friendly_name')
+
     query = None
     categories = None
 
-    if request.GET:
-        # Filter by category
-        if 'category' in request.GET:
-            category_slugs = request.GET.get('category', '').split(',')
-            products = products.filter(category__slug__in=category_slugs)
-            categories = Category.objects.filter(slug__in=category_slugs)
+    # Filter by category
+    if 'category' in request.GET:
+        category_slugs = request.GET.get('category', '').split(',')
+        products = products.filter(category__slug__in=category_slugs)
+        categories = Category.objects.filter(slug__in=category_slugs)
 
-        # Search
-        if 'q' in request.GET:
-            query = request.GET.get('q', '')
-            if not query:
-                messages.error(request, "You didn't enter any search criteria!")
-                return redirect(reverse('products'))
+    # Search functionality
+    if 'q' in request.GET:
+        query = request.GET.get('q', '').strip()
+        if not query:
+            messages.error(request, "You didn't enter any search criteria!")
+            return redirect(reverse('products'))
 
-            products = products.filter(
-                Q(name__icontains=query) |
-                Q(description__icontains=query)
-            )
+        products = products.filter(
+            Q(name__icontains=query) |
+            Q(description__icontains=query)
+        )
 
-        sort = request.GET.get('sort', '')
-        direction = request.GET.get('direction')
+    # Sorting
+    sort = request.GET.get('sort', '')
+    direction = request.GET.get('direction') 
 
-        if sort:
-            sortkey= sort
+    if sort:
+        sortkey = sort
 
-            if sortkey == 'name':
-                products = products.annotate(lower_name=Lower('name'))
-                sortkey = 'lower_name'
+        if sortkey == 'name':
+            products = products.annotate(lower_name=Lower('name'))
+            sortkey = 'lower_name'
+        elif sortkey == 'category':
+            sortkey = 'category__friendly_name'
+            products = products.annotate(lower_category=Lower('category__friendly_name'))
+            sortkey = 'lower_category'
 
-            elif sortkey == 'category':
-                products = products.annotate(lower_category=Lower('category__friendly_name'))
-                sortkey = 'lower_category'
-            
-            if direction == 'desc':
-                sortkey = f'-{sortkey}'
-            
-            products = products.order_by(sortkey)
-        
-        else:
-            products = products.order_by('name')
+        if direction == 'desc':
+            sortkey = f'-{sortkey}'
 
-        current_sorting = f'{sort}_{direction}'
+        products = products.order_by(sortkey)
+    else:
+        products = products.order_by('name')
 
-            
+    current_sorting = f'{sort}_{direction}' if sort else None
+    available_categories = Category.objects.filter(products__in=products).distinct().order_by('friendly_name')
+    
 
     context = {
         'products': products,
         'search_term': query or '',
         'current_categories': categories,
+        'available_categories': available_categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
+
 
 def product_detail(request, product_slug):
     """ A view to show individual product details """
