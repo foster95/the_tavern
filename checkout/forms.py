8befce3bae69f2711a95622b6e283b3de1adb725
foster95 <alicerosefoster95@gmail.com>
2026-02-13 13:43:04 +0000

    update view, form and template for profile

--- checkout/forms.py
+++ checkout/forms.py
@@ -6,41 +6,52 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = (
-            "full_name",
+            "first_name",
+            "last_name",
             "email",
             "phone_number",
             "street_address1",
             "street_address2",
             "town_or_city",
             "county",
-            "country",
             "postcode",
+            "country",
         )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         placeholders = {
-            "full_name": "Full Name",
+            "first_name": "First Name",
+            "last_name": "Last Name",
             "email": "Email Address",
             "phone_number": "Phone Number",
             "street_address1": "Street Address 1",
-            "street_address2": "Street Address 2",
+            "street_address2": "Street Address 2 (optional)",
             "town_or_city": "Town or City",
-            "county": "County, or State",
-            "postcode": "Postal Code",
+            "county": "County (optional)",
+            "postcode": "Postcode",
         }
 
-        self.fields["full_name"].widget.attrs["autofocus"] = True
+        self.fields["first_name"].widget.attrs["autofocus"] = True
+        self.fields["last_name"].widget.attrs["autofocus"] = True
 
         for field_name, field in self.fields.items():
+            # Add your consistent styling class
+            existing_classes = field.widget.attrs.get("class", "")
+            field.widget.attrs["class"] = f"{existing_classes} stripe-style-input".strip()
+
             if field_name != "country":
-                placeholder = placeholders.get(
-                    field_name,
-                    field_name.replace("_", " ").title()
-                    )
+                placeholder = placeholders.get(field_name, field_name.replace("_", " ").title())
+
+                # optional: show * in placeholder if required
                 if field.required:
-                    placeholder = f"{placeholder}"
-                    field.widget.attrs["placeholder"] = placeholder
-                    field.widget.attrs["class"] = "stripe-style-input"
-                    field.label = False
+                    placeholder = f"{placeholder} *"
+
+                field.widget.attrs["placeholder"] = placeholder
+                field.label = False  # hide label for inputs
+
+        # Country select: keep label hidden if you want, but set an empty_label instead
+        self.fields["country"].label = False
+        # If you are using django-countries CountryField, this works:
+        self.fields["country"].empty_label = "Select Country"
---
+++ profiles/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ("user",)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        placeholders = {
+            "default_first_name": "First Name",
+            "default_last_name": "Last Name",
+            "default_phone_number": "Phone Number",
+            "default_street_address1": "Street Address 1",
+            "default_street_address2": "Street Address 2",
+            "default_town_or_city": "Town or City",
+            "default_county": "County / State",
+            "default_postcode": "Postal Code",
+        }
+
+        if "default_first_name" in self.fields:
+            self.fields["default_first_name"].widget.attrs["autofocus"] = True
+
+        for field_name, field in self.fields.items():
+            field.label = False
+
+            field.widget.attrs["class"] = "stripe-style-input"
+
+            if field_name != "default_country":
+                placeholder = placeholders.get(
+                    field_name,
+                    field_name.replace("_", " ").title()
+                )
+                if field.required:
+                    placeholder = f"{placeholder} *"
+                field.widget.attrs["placeholder"] = placeholder
