f6945ab1e560f6b786daa605adab4a898bec32e0
foster95 <alicerosefoster95@gmail.com>
2026-02-16 22:40:22 +0000

    update python version and static root to fix heroku build fail

--- the_tavern/settings.py
+++ the_tavern/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
