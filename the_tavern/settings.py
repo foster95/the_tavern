575e021210203bc63638cd86fd060ac6a09ec693
foster95 <alicerosefoster95@gmail.com>
2026-02-17 10:54:49 +0000

    add home to mobile navigation bar and add aws cache settings

--- the_tavern/settings.py
+++ the_tavern/settings.py
@@ -28,6 +28,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=946080000',
+    }
     AWS_STORAGE_BUCKET_NAME = 'the-tavern'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
