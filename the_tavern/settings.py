9e5fbfe5105db6227b171c91b557136772811ad1
foster95 <alicerosefoster95@gmail.com>
2026-02-26 15:15:10 +0000

    fix heroku build fail

--- the_tavern/settings.py
+++ the_tavern/settings.py
@@ -36,6 +36,8 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = 'the-tavern'
+    AWS_S3_REGION_NAME = 'eu-north-1'
 
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
 
